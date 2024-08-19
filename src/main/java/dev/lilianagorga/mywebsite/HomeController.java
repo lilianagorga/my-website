b8a8ba03175682130c9e07d92481d43b9701febe
Liliana Gorga <gorgaliliana87@gmail.com>
2024-08-19 05:54:01 +0200

    basic setup

---
+++ src/main/java/dev/lilianagorga/mywebsite/HomeController.java
@@ -0,0 +1,13 @@
+package dev.lilianagorga.mywebsite;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+  @RequestMapping("/")
+  public String home(){
+    return "This is home page!";
+  }
+}
