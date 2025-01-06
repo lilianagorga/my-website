77cb9456e89bf3f1dc78599b8aa0718d38286c4d
Liliana Gorga <gorgaliliana87@gmail.com>
2025-01-06 14:46:07 +0100

    Fix production environment configuration to use Render variables

--- src/main/java/dev/lilianagorga/mywebsite/MyWebsiteApplication.java
+++ src/main/java/dev/lilianagorga/mywebsite/MyWebsiteApplication.java
@@ -14,20 +14,21 @@ public class MyWebsiteApplication {
       activeProfile = "dev";
     }
     System.setProperty("spring.profiles.active", activeProfile);
-    String envFileName = activeProfile.equals("dev") ? ".env" : ".env." + activeProfile;
-    Dotenv dotenv = Dotenv.configure().filename(envFileName).load();
+    if (!"prod".equals(activeProfile)) {
+      String envFileName = activeProfile.equals("dev") ? ".env" : ".env." + activeProfile;
+      Dotenv dotenv = Dotenv.configure().filename(envFileName).load();
 
-    String dbUriKey = switch (activeProfile) {
-      case "test" -> "TEST_DB_URL";
-      case "prod" -> "PROD_DB_URL";
-      default -> "DB_URL";
-    };
+      String dbUriKey = switch (activeProfile) {
+        case "test" -> "TEST_DB_URL";
+        default -> "DB_URL";
+      };
 
-    String dbUri = dotenv.get(dbUriKey);
-    if (dbUri == null) {
-      throw new IllegalStateException("Variable " + dbUriKey + " not found into file " + envFileName);
+      String dbUri = dotenv.get(dbUriKey);
+      if (dbUri == null) {
+        throw new IllegalStateException("Variable " + dbUriKey + " not found in file " + envFileName);
+      }
+      System.setProperty("spring.data.mongodb.uri", dbUri);
     }
-    System.setProperty("spring.data.mongodb.uri", dbUri);
 
     SpringApplication.run(MyWebsiteApplication.class, args);
   }
