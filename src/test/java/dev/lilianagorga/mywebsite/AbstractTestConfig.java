61a81ec113967a3cb410a62f082a8f84f2311961
Liliana Gorga <gorgaliliana87@gmail.com>
2025-01-06 14:09:23 +0100

    Configured database connection and environment profiles (dev, test and prod)

---
+++ src/test/java/dev/lilianagorga/mywebsite/AbstractTestConfig.java
@@ -0,0 +1,18 @@
+package dev.lilianagorga.mywebsite;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class AbstractTestConfig {
+
+  @DynamicPropertySource
+  static void configureDynamicProperties(DynamicPropertyRegistry registry) {
+    Dotenv dotenv = Dotenv.configure().filename(".env.testing").load();
+    registry.add("spring.data.mongodb.uri", () -> dotenv.get("TEST_DB_URL"));
+  }
+}
--- src/test/java/dev/lilianagorga/mywebsite/MyWebsiteApplicationTests.java
+++ src/test/java/dev/lilianagorga/mywebsite/MyWebsiteApplicationTests.java
@@ -2,9 +2,11 @@ package dev.lilianagorga.mywebsite;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class MyWebsiteApplicationTests {
+@ActiveProfiles("test")
+class MyWebsiteApplicationTests extends AbstractTestConfig {
 
   @Test
   void contextLoads() {
