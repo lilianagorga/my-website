a90e21851bc8c9c6406debd71c90eff163cf36c1
Liliana Gorga <gorgaliliana87@gmail.com>
2025-01-07 16:31:26 +0100

    feat: add database seeder, MongoDB config, and snake_case field annotations
    - Added DatabaseSeeder for populating development database with initial data
    - Implemented MongoConfig to remove '_class' field from MongoDB documents
    - Updated entities to use @Field annotation for snake_case naming in MongoDB

--- src/main/java/dev/lilianagorga/mywebsite/entities/Project.java
+++ src/main/java/dev/lilianagorga/mywebsite/entities/Project.java
@@ -3,6 +3,7 @@ package dev.lilianagorga.mywebsite.entities;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.List;
 
@@ -15,12 +16,16 @@ public class Project {
 
   @Id
   private String id;
-
+  @Field("title")
   private String title;
+  @Field("description")
   private String description;
+  @Field("image_url")
   private String imageUrl;
+  @Field("tech_stack")
   private List<String> techStack;
-
+  @Field("github_url")
   private String githubUrl;
+  @Field("demo_url")
   private String demoUrl;
 }
